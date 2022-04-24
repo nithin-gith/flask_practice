5c7a98e4b0e617a502d79e6e6b018f5fa2b17b75
Nithin Siriguppa <nithin.siriguppa@gmail.com>
2022-04-25 02:21:07 +0530

    using layout.html and added bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,3 +1,4 @@
+from opcode import hascompare
 from flask import Flask,render_template
 import json
 app = Flask(__name__)
@@ -5,13 +6,15 @@ app = Flask(__name__)
 data_file = open('data.json',"r")
 data = json.loads(data_file.read()) 
 
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('index.html',posts=data)
+    return render_template('index.html',posts=data,title= "Home")
 
 @app.route('/about')
 def about():
-    return render_template('about.html') 
+    return render_template('about.html',title ="About") 
 
 if __name__ == '__main__':
     app.run(debug=True)
