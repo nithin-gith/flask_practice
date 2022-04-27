1ad4437be77099d9fd6989b51023fd7968edcf6b
Nithin Siriguppa <nithin.siriguppa@gmail.com>
2022-04-27 14:07:02 +0530

    added register and login pages and fuctionality partially

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,8 @@
-from opcode import hascompare
-from flask import Flask,render_template
+from flask import Flask, flash, redirect,render_template, url_for
+from forms import RegistrationForm,LoginForm
 import json
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ENVIRONMENT_VARIABLE'
 
 data_file = open('data.json',"r")
 data = json.loads(data_file.read()) 
@@ -15,6 +16,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title ="About") 
+
+@app.route('/register',methods =['GET','POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title ="Register", form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title ="Registration", form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
